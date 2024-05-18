7e44c093144d3ad3d3ca3af7ea71d823dc1cd1cd
toilaluan <toilaluan@github.com>
2024-05-17 22:51:38 -1100

    up

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 import requests
+import httpx
+import time
 
 
 def test():
-    response = requests.post('http://localhost:10006/generate', json={"prompt": "a", "seed": 0, "pipeline_type": "txt2img"})
-    print(response.json())
+    start = time.time()
+    with httpx.Client(timeout=100) as client:
+        response = client.post('http://localhost:10006/generate', json={"prompt": "a", "seed": 0, "pipeline_type": "txt2img", "pipeline_params": {"num_inference_steps": 15}})
+    end = time.time()
+    print(f"Time taken: {end-start}")
 
 def run_concurrently():
     import threading
-    for _ in range(100):
-        threading.Thread(target=test).start()
-
+    threads = []
+    for i in range(10):
+        thread = threading.Thread(target=test)
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
 if __name__ == "__main__":
     run_concurrently()
